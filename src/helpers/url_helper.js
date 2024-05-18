55b1e10c419ebcc7c9f3f958f733c132aa268736
Uzair <uzair.ejaz2001@gmail.com>
2024-05-19 01:40:33 +0500

    feat: added font

--- src/helpers/url_helper.js
+++ src/helpers/url_helper.js
@@ -16,16 +16,16 @@ export const GET_UNIQUE_PARENTS = 'get-unique-parents';
 export const GET_ALL_ROLES = 'get-all-roles';
 export const CREATE_ROLE = 'create-role';
 export const EDIT_ROLE = 'update-role';
-export const DELETE_ROLE = '/delete-role';
-export const GET_PERMISSIONS = '/get-permissions';
+export const DELETE_ROLE = 'delete-role';
+export const GET_PERMISSIONS = 'get-permissions';
 
 // ADMIN ENDPOINT(S)
 export const GET_ALL_ADMINS = 'get-all-admins';
 export const CREATE_ADMIN = 'create-admin';
 export const EDIT_ADIMN = 'update-admin';
-export const DELETE_ADMIN = '/delete-admin';
-export const UPDATE_PASSWORD = '/get-permissions';
-export const GET_UNIQUE_ROLES = '/get-unique-roles';
+export const DELETE_ADMIN = 'delete-admin';
+export const UPDATE_PASSWORD = 'get-permissions';
+export const GET_UNIQUE_ROLES = 'get-unique-roles';
 
 // USER ENDPOINT(S)
 export const GET_ALL_USERS = 'get-all-users';
