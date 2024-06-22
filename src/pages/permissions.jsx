dabb6382877727ccd4a039ba7306f52d9cdeaa21
Uzair <uzair.ejaz2001@gmail.com>
2024-06-22 14:24:42 +0500

    feat: add dispatch for permissions page

--- src/pages/permissions.jsx
+++ src/pages/permissions.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import Head from 'next/head';
 import { Col, Container, Row, Card, CardHeader, UncontrolledTooltip } from 'reactstrap';
 import { useDispatch, useSelector } from 'react-redux';
@@ -46,6 +46,10 @@ const Permissions = () => {
   const deletePermission = () => {
     dispatch(permissionThunk.deletePermission({ permissionToDelete, setDeleteModal, refetch }));
   };
+
+  useEffect(() => {
+    dispatch(permissionThunk.getAllPermissions(filters));
+  }, [filters, fetch]);
 
   const actionBtns = _ => (
     <>
